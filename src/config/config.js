39e11946feadcf67cb7ab6c91be0d5e32c44c989
Mo <bakhtiar.mojtabaqgmail.com>
2021-10-19 12:28:59 +0200

    feat: add database config

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const creds = {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOSTNAME,
+      dialect: 'postgresql'
+};
+  
+module.exports = creds;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const config = require("./config");
+
+module.exports = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  { host: config.host, 
+    dialect: config.dialect
+  }
+);
