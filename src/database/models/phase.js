4b2a67497bae82ae43d8f81343ca16dd16df5abb
Hosna mohebi <hosna@m.lo.agency>
2021-10-22 21:56:05 +0330

    feat: Update models association

--- src/database/models/phase.js
+++ src/database/models/phase.js
@@ -7,8 +7,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      this.belongsTo(models.Roadmap);
+    static associate({ Roadmap }) {
+      this.belongsTo(Roadmap, {
+        foreignKey: "roadmap_id",
+        as: "roadmap",
+      });
     }
   }
   Phase.init(
--- src/database/models/roadmap.js
+++ src/database/models/roadmap.js
@@ -7,8 +7,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      this.hasMany(models.Phase, { foreignKey: "roadmap_id" });
+    static associate({ Phase }) {
+      this.hasMany(Phase, { foreignKey: "roadmap_id", as: "phases" });
     }
   }
   Roadmap.init(
