777b916cf31a327257ca46f70b77a9f41a28d5c1
Mo <bakhtiar.mojtabaqgmail.com>
2021-10-31 19:39:05 +0100

    feat: add phase controller

---
+++ src/controllers/phase.js
@@ -0,0 +1,97 @@
+"use strict";
+require("dotenv").config();
+const { Logger } = require("@lo-agency/logger");
+const { Phase } = require("../database/models");
+
+module.exports = {
+  getAllPhases: async (req, res) => {
+    try {
+      const phases = await Phase.findAll();
+      return res.status(200).json(phases);
+    } catch (error) {
+      Logger.error(error.message);
+      return res.status(500).json(error.message);
+    }
+  },
+
+  getPhase: async (req, res) => {
+    const uuid = req.params.id;
+    try {
+      const phase = Phase.findOne({
+        where: { uuid },
+      });
+      if (phase === null) {
+        Logger.warn(`phase not found by id: ${uuid}`);
+        return res.status(404).json({ msg: `phase not found by id: ${uuid}` });
+      }
+      return res.status(200).json(phase);
+    } catch (error) {
+      Logger.error(error.message);
+      return res.status(500).json(error.message);
+    }
+  },
+
+  insertPhase: async (req, res) => {
+    const { name, start_date, end_date, evaluation_date, learning_days } =
+      req.body;
+
+    try {
+      const newPhase = await Phase.create({
+        name,
+        start_date,
+        end_date,
+        evaluation_date,
+        learning_days,
+      });
+
+      return res.status(201).json(newPhase);
+    } catch (error) {
+      Logger.error(error.message);
+      return res.status(500).json(error.message);
+    }
+  },
+
+  updatePhase: async (req, res) => {
+    const uuid = req.params.id;
+    try {
+      const phase = await Phase.findOne({ where: uuid });
+      if (phase === null) {
+        Logger.warn(`phase not found by id: ${uuid}`);
+        return res.status(404).json({ msg: `phase not found by id: ${uuid}` });
+      }
+
+      await phase.update(req.body, {
+        where: {
+          uuid: phase.uuid,
+        },
+      });
+
+      return res.status(200).json({ msg: "phase is changed", phase });
+    } catch (error) {
+      Logger.error(error.message);
+      return res.status(500).json(error.message);
+    }
+  },
+
+  deletePhase: async (req, res) => {
+    const uuid = req.params.id;
+    try {
+      const phase = await Phase.findOne({ where: uuid });
+      if (phase === null) {
+        Logger.warn(`phase not found by id: ${uuid}`);
+        return res.status(404).json({ msg: `phase not found by id: ${uuid}` });
+      }
+
+      await Phase.destroy({
+        where: {
+          uuid: phase.uuid,
+        },
+      });
+
+      return res.status(200).json({ msg: "phase is deleted" });
+    } catch (error) {
+      Logger.error(error.message);
+      return res.status(500).json(error.message);
+    }
+  },
+};
