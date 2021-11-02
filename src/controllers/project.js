f8006a5c91a0f06f337d5442d6f8d76f87be3ce6
Mo <bakhtiar.mojtabaqgmail.com>
2021-11-02 18:18:17 +0100

    feat: add put and delete function to project controller

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   getProject: async (req, res) => {
-    const uuid = req.params.pid; // edit this
+    const uuid = req.params.prId;
     try {
       const project = await Project.findOne({ where: { uuid } });
 
@@ -34,16 +34,59 @@ module.exports = {
 
   insertProject: async (req, res) => {
     const { name, start_date, end_date, type } = req.body;
-    console.log(req.params);
     try {
       const newProject = await Project.create({
-        phase_id: req.params.pid,
         name,
+        phase_id: req.params.pid,
         start_date,
         end_date,
         type,
       });
       return res.status(201).json(newProject);
+    } catch (error) {
+      Logger.error(error.message);
+      return res.status(500).json(error.message);
+    }
+  },
+
+  updateProject: async (req, res) => {
+    const uuid = req.params.prId;
+    try {
+      const project = await Project.findOne({ where: { uuid } });
+      if (project === null) {
+        Logger.warn(`Project not found by id: ${uuid}`);
+        return res
+          .status(404)
+          .json({ msg: `Project not found by id: ${uuid}` });
+      }
+
+      await project.update(req.body, {
+        where: {
+          uuid: project.uuid,
+        },
+      });
+
+      return res.status(200).json({ msg: "project is changed", project });
+    } catch (error) {
+      Logger.error(error.message);
+      return res.status(500).json(error.message);
+    }
+  },
+
+  deleteProject: async (req, res) => {
+    const uuid = req.params.prId;
+    try {
+      const project = await Project.findOne({ where: { uuid } });
+      if (project === null) {
+        Logger.warn(`Project not found by id: ${uuid}`);
+        return res
+          .status(404)
+          .json({ msg: `Project not found by id: ${uuid}` });
+      }
+
+      await project.destroy();
+
+      return res.status(200).json({ msg: "project is deleted" });
     } catch (error) {
       Logger.error(error.message);
       return res.status(500).json(error.message);
