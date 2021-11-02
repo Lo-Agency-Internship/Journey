57fd63459d978af829cf5a0859cc724e885f95a8
Mo <bakhtiar.mojtabaqgmail.com>
2021-11-02 10:45:46 +0100

    refactor: edit the type of roadmap_id

--- src/database/migrations/20211019142853-create-phase.js
+++ src/database/migrations/20211019142853-create-phase.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       roadmap_id: {
         allowNull: false,
-        type: Sequelize.INTEGER,
+        type: Sequelize.UUID,
       },
       start_date: {
         allowNull: false,
