d8852c36da6417a24f8b857d875ff73d3f961b17
Mo <bakhtiar.mojtabaqgmail.com>
2021-11-03 06:55:29 +0100

    feat: add route of resource

---
+++ src/routes/resource.js
@@ -0,0 +1,25 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const resourceRouter = express.Router();
+const {
+  getAllResources,
+  getResource,
+  insertResource,
+  updateResource,
+  deleteResource,
+} = require("../controllers/resource");
+
+const isAuth = require("../middleware/is-auth");
+
+resourceRouter.get("/", isAuth, getAllResources);
+
+resourceRouter.get("/:rId", isAuth, getResource);
+
+resourceRouter.post("/", isAuth, insertResource);
+
+resourceRouter.put("/:rId", isAuth, updateResource);
+
+resourceRouter.delete("/:rId", isAuth, deleteResource);
+
+module.exports = resourceRouter;
